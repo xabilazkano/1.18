728c63c7da4cc03655afd1dcbc24b5eac9d7a9da
Xabi <xlazcanoit18dw@ikzubirimanteo.com>
2018-10-26 10:27:16 +0200

    26/10

--- src/Guess.java
+++ src/Guess.java
@@ -10,38 +10,37 @@ public class Guess {
 		int tries=0;
 		int number = ran.nextInt(200-1)+1;
 		String num = String.valueOf(number);
-		System.out.println("I will think a number between 100 and 200, try to guess it in less than 10 tries.");
+		System.out.println("I will think a number between 1 and 200, try to guess it in less than 10 tries.");
 		
 		while (flag==0) {
-			switch(tries) {
-				default:
-					System.out.println("Tell me a number");
-					if (sc.hasNextInt()) {
-						int number2=sc.nextInt();
-						if (number2>=1 && number2<=200) {
-							if (number<number2) {
-								System.out.println("My number is smaller");
-								tries++;
-							}
-							else if (number>number2) {
-								System.out.println("My number is bigger");
-								tries++;
-							}
-							else {
-								tries++;
-								System.out.println("You guessed it in " + tries + " tries.");
-								flag++;
-							}
-						}
-						else {
-							System.out.println("You haven't entered a number between 1 and 200");
-						}						
+			System.out.println("Tell me a number");
+			if (sc.hasNextInt()) {
+				int number2=sc.nextInt();
+				if (number2>=1 && number2<=200) {
+					if (number<number2) {
+						System.out.println("My number is smaller");
+						tries++;
+					}
+					else if (number>number2) {
+						System.out.println("My number is bigger");
+						tries++;
 					}
 					else {
-						System.out.println("You haven't entered a number");
-						sc.next();
+						tries++;
+						System.out.println("You guessed it in " + tries + " tries.");
+						flag++;
 					}
-					break;
+				}
+				else {
+					System.out.println("You haven't entered a number between 1 and 200");
+				}						
+			}
+			else {
+				System.out.println("You haven't entered a number");
+				sc.next();
+			}
+			switch(tries) {
+				
 				
 				case 3:
 					System.out.println("I will give you a hint");
@@ -56,36 +55,11 @@ public class Guess {
 					else {
 						System.out.println("My number isn't palindrome");
 					}
-					System.out.println("Tell me a number");
-					if (sc.hasNextInt()) {
-						int number2=sc.nextInt();
-						if (number2>=1 && number2<=200) {
-							if (number<number2) {
-								System.out.println("My number is smaller");
-								tries++;
-							}
-							else if (number>number2) {
-								System.out.println("My number is bigger");
-								tries++;
-							}
-							else {
-								tries++;
-								System.out.println("You guessed it in " + tries + " tries.");
-								flag++;
-							}
-						}
-						else {
-							System.out.println("You haven't entered a number between 1 and 200");
-						}						
-					}
-					else {
-						System.out.println("You haven't entered a number");
-						sc.next();
-					}
+					
 					break;
 					
 				case 5:
-					System.out.println("I will give you my last hint");
+					System.out.println("I will give you another hint");
 					if (number<100) {
 					System.out.println("The first digit is " + num.charAt(0));
 					}
@@ -97,36 +71,11 @@ public class Guess {
 							System.out.println("My number is odd");
 						}
 					}
-					System.out.println("Tell me a number");
-					if (sc.hasNextInt()) {
-						int number2=sc.nextInt();
-						if (number2>=1 && number2<=200) {
-							if (number<number2) {
-								System.out.println("My number is smaller");
-								tries++;
-							}
-							else if (number>number2) {
-								System.out.println("My number is bigger");
-								tries++;
-							}
-							else {
-								tries++;
-								System.out.println("You guessed it in " + tries + " tries.");
-								flag++;
-							}
-						}
-						else {
-							System.out.println("You haven't entered a number between 1 and 200");
-						}						
-					}
-					else {
-						System.out.println("You haven't entered a number");
-						sc.next();
-					}
+					
 					break;
 					
 				case 8:
-					System.out.println("I will give you another hint");
+					System.out.println("I will give you my last hint");
 					if (number>=100) {
 						System.out.println("The last digit is " + num.charAt(num.length()-1));
 					}
@@ -138,32 +87,7 @@ public class Guess {
 							System.out.println("My number is odd");
 						}
 					}
-					System.out.println("Tell me a number");
-					if (sc.hasNextInt()) {
-						int number2=sc.nextInt();
-						if (number2>=1 && number2<=200) {
-							if (number<number2) {
-								System.out.println("My number is smaller");
-								tries++;
-							}
-							else if (number>number2) {
-								System.out.println("My number is bigger");
-								tries++;
-							}
-							else {
-								tries++;
-								System.out.println("You guessed it in " + tries + " tries.");
-								flag++;
-							}
-						}
-						else {
-							System.out.println("You haven't entered a number between 1 and 200");
-						}						
-					}
-					else {
-						System.out.println("You haven't entered a number");
-						sc.next();
-					}
+					
 					break;
 					
 				case 10:
